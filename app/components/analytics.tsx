5c663c96c0e58310b72c413e9ed3a677c0e0c5fd
dabit3 <dabit3@gmail.com>
2023-02-06 04:23:48 -0600

    added analytics

---
+++ app/components/analytics.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { Analytics } from '@vercel/analytics/react';
+
+export function AnalyticsWrapper() {
+  return <Analytics />;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { AnalyticsWrapper } from './components/analytics';
 
 export default function RootLayout({
   children,
@@ -12,7 +13,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        {children}
+        <AnalyticsWrapper />
+      </body>
     </html>
   )
 }
